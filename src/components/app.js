f8692cfb6b2cf212891148468b53763e4a877e54
Michael Pascuzzi <gatorpazz@gmail.com>
2018-06-04 10:34:54 -0400

    First commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import SearchBarContainer from '../containers/SearchBarContainer';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBarContainer />
+      </div>
+    );
+  }
+}
